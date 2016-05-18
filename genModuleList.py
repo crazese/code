d8fba61d3310cf275506f47e250dff8620834438
crazese <hustdwcz@gmail.com>
2016-05-19 00:18:32 +0800

    Create genModuleList.py
    文件导入的所有模块

---
+++ genModuleList.py
@@ -0,0 +1,28 @@
+    import os
+
+    KEYWORD = 'import'
+    def modulesList(filename,modules = []):
+        path = os.getcwd()
+        file = os.path.join(path,filename)
+        print file
+        fobj = open(file,'r')
+        for line in fobj.readlines():
+            if KEYWORD in line:
+                linewithoutSpace = line.strip().replace(' ','')
+                print line, linewithoutSpace
+                print "this is a modules"
+                index = linewithoutSpace.find(KEYWORD)
+                if 'from' in line:
+                # from ..import..
+                    indexFrom = linewithoutSpace.find('from')
+                    tempMod1 = linewithoutSpace[indexFrom+4:index]
+                    modules.append(tempMod1)
+                else:
+                # import ..
+                    tempMods1 = linewithoutSpace[index+6:].split(',')
+                    modules = modules+tempMods1
+        return list(set(modules))
+
+
+    if __name__ == '__main__':
+        print modulesList('test.py',['os'])
